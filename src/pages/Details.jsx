3610348085897cb8c7cad2b8de6f5df579f646f9
Leonardo <leu1.g.carvalho@gmail.com>
2024-07-30 14:34:56 -0300

    Teste React

---
+++ src/pages/Details.jsx
@@ -0,0 +1,15 @@
+export function Details(){
+  return(
+    <>
+      <h1>Hello, World!</h1>
+      <div>
+        <p>Meu nome é Leo Carvalho</p>
+      </div>
+      <ul>
+        <li>bolo</li>
+        <li>cookies</li>
+        <li>refri</li>
+      </ul>
+    </>
+  )
+}
