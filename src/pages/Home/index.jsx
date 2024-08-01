fc33cf46382573ae5ef7823b6afac475fdcb9a47
Leonardo <leu1.g.carvalho@gmail.com>
2024-08-01 17:28:34 -0300

    Add Note and Input Improve

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,11 @@
-import { FiPlus } from "react-icons/fi";
+import { FiPlus, FiSearch } from "react-icons/fi";
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 
 import { Header } from "../../components/Header";
 import { ButtonText } from "../../components/ButtonText";
+import { Section } from "../../components/Section";
+import { Note } from "../../components/Note";
+import { Input } from "../../components/Input";
 
 export function Home(){
     return (
@@ -20,11 +23,19 @@ export function Home(){
             </Menu>
 
             <Search>
-
+                <Input placeholder="Pesquisar pelo título" icon={FiSearch} />
             </Search>
 
             <Content>
-
+                <Section title="Minhas notas">
+                    <Note data={{
+                        title: 'React',
+                        tags: [
+                            { id: '1', name: 'react' }, 
+                            {id: '2', name: 'rocket'}  
+                        ]
+                        }}/>
+                </Section>
             </Content>
 
             <NewNote>
