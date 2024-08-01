7bd1f120809d6f5f9990bce1e3d950b4b36ebd6c
Leonardo <leu1.g.carvalho@gmail.com>
2024-08-01 14:54:01 -0300

    Finishing the Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,21 @@
-import { Container } from './styles';
+import { RiShutDownLine } from 'react-icons/ri';
+import { Container, Profile, Logout } from './styles';
 
 export function Header(){
     return(
         <Container>
-            
+            <Profile>
+                <img src="https://github.com/LeoGCarva.png" alt="Foto do usuário" />
+
+                <div>
+                    <span>Bem-Vindo</span>
+                    <strong>Leo Carvalho</strong>
+                </div>
+            </Profile>
+
+            <Logout>
+                <RiShutDownLine />
+            </Logout>
         </Container>
     )
 }
