71c7faf4aec0e2fbe4a24a7e89a0e1c73bf48070
Leonardo <leu1.g.carvalho@gmail.com>
2024-08-04 17:04:36 -0300

    Finishing "New" page

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -5,6 +5,7 @@ import { Input } from "../../components/Input";
 import { Textarea } from "../../components/Textarea";
 import { NoteItem } from "../../components/NoteItem";
 import { Section }  from "../../components/Section";
+import { Button } from "../../components/Button"
 
 export function New(){
     return(
@@ -25,6 +26,15 @@ export function New(){
                         <NoteItem value="https://github.com"/>
                         <NoteItem isNew placeholder="Novo link"/>
                     </Section>
+
+                    <Section title="Marcadores">
+                        <div className="tags">
+                            <NoteItem value="React"/>
+                            <NoteItem isNew placeholder="Novo tag"/>
+                        </div>
+                    </Section>
+
+                    <Button title="Salvar" />
                 </Form>
             </main>
         </Container>
